062e63ce33a1ac6b57fc0f9c4d4e79de29e3559e
RedPhoenixDev <raye@RAYE-ASUS>
2014-09-04 21:43:45 +0300

    Initial commit

---
+++ Prison Architect-android/src/com/redphoenix/prison/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.redphoenix.prison.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.redphoenix.prison.PrisonArchitect;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new PrisonArchitect(), config);
+	}
+}
